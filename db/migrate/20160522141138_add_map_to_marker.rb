7f772967374edba8bf3f61c242c2cef4ec824b8a
Ricardo Campos <ricardocamposp@gmail.com>
2016-05-30 19:33:37 +0000

    Deleted migrates referencing the 'Map' entity

--- db/migrate/20160522141138_add_map_to_marker.rb
+++
@@ -1,5 +0,0 @@
-class AddMapToMarker < ActiveRecord::Migration
-  def change
-    add_reference :markers, :map, index: true, foreign_key: true
-  end
-end
--- db/migrate/20160522152617_remove_reference.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReference < ActiveRecord::Migration
-  def change
-  	remove_reference(:markers, :map, index: true, foreign_key: true)
-  end
-end
